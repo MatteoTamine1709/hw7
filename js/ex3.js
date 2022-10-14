1877f6727123f605c389f56ca64546627db4bb85
Matteo Tamine <matteo.tamine@epitech.eu>
2022-10-14 04:20:47 +0200

    hw7

---
+++ js/ex3.js
@@ -0,0 +1,12 @@
+// Select all paragraphs
+const paragraphs = document.querySelectorAll('p');
+
+const changeNodesStyle = (nodes, properties) => {
+    for (let i = 0; i < nodes.length; i++) {
+        for (let prop of properties) {
+            nodes[i].style[prop.property] = prop.value;
+        }
+    }
+}
+
+changeNodesStyle(paragraphs, [{ property: 'backgroundColor', value: 'yellow'}, { property: 'fontWeight', value: 'bold'}]);
---
+++ js/ex5.js
@@ -0,0 +1,12 @@
+const output = document.getElementById('output');
+const table = document.createElement('table');
+
+for (let i = 1; i <= 12; i++) {
+    const row = document.createElement('tr');
+    const cell = document.createElement('td');
+    cell.textContent = i;
+    row.appendChild(cell);
+    table.appendChild(row);
+}
+
+output.appendChild(table);
